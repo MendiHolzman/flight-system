1bd6d4237c12c79bcc6a38eeada8ead9a3c8a4df
MendiHolzman <mmh7701work@gmail.com>
2022-12-22 20:08:43 +0200

    VV

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,17 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { MainComponent } from './components/main/main.component';
 import { AudioComponent } from './components/audio/audio.component';
+import { BodyComponent } from './components/body/body.component';
 
 
 
 @NgModule({
-  declarations: [MainComponent, AudioComponent],
+  declarations: [MainComponent, AudioComponent, BodyComponent],
   imports: [
     CommonModule,
   ],
   exports:[
     MainComponent,
-    AudioComponent
+    AudioComponent,
+    BodyComponent
   ]
 })
 export class HomeModule { }
